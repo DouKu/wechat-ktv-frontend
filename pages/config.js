dd50b81f7e4b53b634340df8dd53c61154878d35
lgybetter <437675103@qq.com>
2017-10-23 22:30:07 +0800

    test

--- pages/config.js
+++ pages/config.js
@@ -1,7 +1,7 @@
 export default {
   appId: 'wxed73c69d3bdf1dce',
   redirectUrl: 'http://znq.gzmylike.com',
-  baseUrl: 'http://znq.gzmylike.com:3002',
+  baseUrl: 'http://znq.gzmylike.com:443',
   recordId: '13213',
   chorusId: '',
   dev: false
