f7cb6bbe0c175fef0b904b503e23f14ac37189af
lgybetter <437675103@qq.com>
2017-10-23 21:58:39 +0800

    change baseUrl

--- pages/config.js
+++ pages/config.js
@@ -1,7 +1,7 @@
 export default {
   appId: 'wxed73c69d3bdf1dce',
   redirectUrl: 'http://znq.gzmylike.com',
-  baseUrl: 'http://znq.gzmylike.com',
+  baseUrl: 'http://znq.gzmylike.com:3001',
   recordId: '13213',
   chorusId: '',
   dev: false
