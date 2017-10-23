2d4b4a1e6a7a23f32b8abea5e24479a1621195b5
lgybetter <437675103@qq.com>
2017-10-23 22:15:55 +0800

    test

--- pages/config.js
+++ pages/config.js
@@ -1,7 +1,7 @@
 export default {
   appId: 'wxed73c69d3bdf1dce',
   redirectUrl: 'http://znq.gzmylike.com',
-  baseUrl: 'http://39.106.51.178:3001',
+  baseUrl: 'http://znq.gzmylike.com:3002',
   recordId: '13213',
   chorusId: '',
   dev: false
