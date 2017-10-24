2bbacb3c7497db6d6f9185742ea132f1f0159c20
lgybetter <437675103@qq.com>
2017-10-24 22:26:11 +0800

    fix the auth

--- pages/index.vue
+++ pages/index.vue
@@ -37,6 +37,7 @@ export default {
       })
       wx.config(res.data.data)
       wx.ready(function () {
+        config.auth = true
         console.log('config success')// config信息验证后会执行ready方法，所有接口调用都必须在config接口获得结果之后，config是一个客户端的异步操作，所以如果需要在页面加载时就调用相关接口，则须把相关接口放在ready函数中调用来确保正确执行。对于用户触发时才调用的接口，则可以直接调用，不需要放在ready函数中。
       })
       wx.error(function (res) {
@@ -73,7 +74,6 @@ export default {
           // 用户取消分享后执行的回调函数
         }
       })
-      config.auth = true
     }
     const wechatCode = this.$route.query.code
     const saveOpenId = window.localStorage.getItem('openid')
@@ -81,7 +81,6 @@ export default {
       window.localStorage.setItem('redirect', this.$route.query.redirect)
     }
     if (!dev) {
-
       if (!saveOpenId && !wechatCode) {
         this.showToAuth = true
       } else if (wechatCode && !saveOpenId) {
