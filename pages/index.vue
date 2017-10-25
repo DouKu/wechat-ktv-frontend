c5fa2844f7c7cd03aa605a484a6c1b1fbf89474d
lgybetter <437675103@qq.com>
2017-10-26 00:06:52 +0800

    test

--- pages/index.vue
+++ pages/index.vue
@@ -79,7 +79,6 @@ export default {
           })
           wx.config(res.data.data)
           wx.ready( () => {
-            config.auth = true
             wx.onMenuShareTimeline({
               title: '美莱周年庆', // 分享标题
               link: `${config.redirectUrl}?type=share`, // 分享链接，该链接域名或路径必须与当前页面对应的公众号JS安全域名一致
@@ -110,14 +109,14 @@ export default {
                 // 用户取消分享后执行的回调函数
               }
             })
+            config.auth = true
             console.log('config success')// config信息验证后会执行ready方法，所有接口调用都必须在config接口获得结果之后，config是一个客户端的异步操作，所以如果需要在页面加载时就调用相关接口，则须把相关接口放在ready函数中调用来确保正确执行。对于用户触发时才调用的接口，则可以直接调用，不需要放在ready函数中。
           })
-          config.auth = true
+          wx.error(function (res) {
+            // config信息验证失败会执行error函数，如签名过期导致验证失败，具体错误信息可以打开config的debug模式查看，也可以在返回的res参数中查看，对于SPA可以在这里更新签名。
+            console.log('wx jsapi err:', res)
+          })
         }
-        wx.error(function (res) {
-          // config信息验证失败会执行error函数，如签名过期导致验证失败，具体错误信息可以打开config的debug模式查看，也可以在返回的res参数中查看，对于SPA可以在这里更新签名。
-          console.log('wx jsapi err:', res)
-        })
       }
     }
   }
